c97b93009f26bb9cc8716b9626fe4847da228bb8
Vinay kumar Godavarti <gvk.kumar100@gmail.com>
2026-02-08 11:53:01 -0500

    Had a small confusion cleared about Factory Pattern

--- Mixed-Design-Patterns/ECommerceOrderProcessingSystem/Paymentfactory.java
+++ Mixed-Design-Patterns/ECommerceOrderProcessingSystem/Paymentfactory.java
@@ -17,4 +17,12 @@ public class Paymentfactory {
 //Static - Belongs to class not to an object of class
 //Fields are also static because if not then we cannot send those variable to outside class without creating object of Paymentfactory class
 //A public method can provide controlled access to private fields or methods from outside the class.
-// This is called encapsulation—it hides internal details and exposes only what’s necessary.
+// This is called encapsulation—it hides internal details and exposes only what’s necessary. 
+
+// Factory created stateegies are stateless because they don't maintain any internal state that changes over time. So, safe to share among with multiple threads(Users) without synchronization issues.
+// Stateless - No internal state, so it can be shared across multiple threads without synchronization issues.
+// Here, CreditCard and PayPal are stateless because they don't maintain any internal state that changes over time. 
+// They simply implement the PaymentStrategy interface and provide a way to process payments. 
+// This allows us to create a single instance of each payment method and reuse it across the application without worrying about thread safety or synchronization issues.
+
+// Stateful - Maintains internal state that can change over time, which may require synchronization when accessed by multiple threads.
